2549c038c84c88d4d68f531810417445d5a9f93b
maycon-douglas2005 <mayconchaves2005@gmail.com>
2025-10-03 17:02:18 -0300

    feat: header criado

---
+++ app/Views/partials/header.php
@@ -0,0 +1,13 @@
+<header class="container-fluid  d-flex flex-row justify-content-between">
+    <div class="logo row-6">
+        <a class="col-auto btn fw-bold fs-5" href="<?= $baseUrl ?>">Lista De Contatos</a>
+    </div>
+    <nav class="row-6">
+        <ul class="col-auto list-unstyled d-flex flex-row">
+            <li class="mx-2 btn"><a href="" class="text-dark text-decoration-none btn btn-outline-secondary">Home</a></li>
+            <li class="mx-2 btn"><a href="" class="text-dark text-decoration-none btn btn-outline-secondary">Cadastro</a></li>
+            <li class="mx-2 btn"><a href="" class="text-dark text-decoration-none btn btn-outline-secondary">Login</a></li>
+        </ul>
+    </nav>
+</header>
+<hr class="mt-0">
--- public/index.php
+++ public/index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Contatos</title>
-</head>
+<?php require_once '../app/Views/partials/head.php';
+$baseUrl = '/Projetos de Programação/lista_de_contatos/public/index.php';
+?>
+
+
 <body>
-    
+<?php require_once '../app/Views/partials/header.php' ?>
+
+
+
+
+
+
+
+<?php require_once '../app/Views/partials/footer.php' ?>
+
+
 </body>
 </html>
