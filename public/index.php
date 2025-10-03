5c159dc36a5c73eb4b470e027cc158faccbdd15d
maycon-douglas2005 <mayconchaves2005@gmail.com>
2025-10-03 17:20:30 -0300

    feat: index.php finalizado

--- public/index.php
+++ public/index.php
@@ -4,16 +4,29 @@ $baseUrl = '/Projetos de Programação/lista_de_contatos/public/index.php';
 
 
 <body>
-<?php require_once '../app/Views/partials/header.php' ?>
+    <?php require_once '../app/Views/partials/header.php' ?>
 
 
 
+    <main class="container-fluid">
+        <div class="apresentacao row d-flex flex-row align-items-center justify-content-center">
+
+            <section class="tituloEparagrafo col-6">
+                <h1>Bem-Vindo A Sua Lista De Contatos</h1>
+                <p>Gerencie seus contatos de maneira simples e eficiente</p>
+                <button class="btn btn-outline-primary">Cadastrar-se</button>
+                <button class="btn btn-outline-primary">Logar</button>
+            </section>
+
+            <img src="images/home_site.png" class="img-fluid col-6" alt="">
+
+    </main>
 
 
 
-
-<?php require_once '../app/Views/partials/footer.php' ?>
+    <?php require_once '../app/Views/partials/footer.php' ?>
 
 
 </body>
+
 </html>
