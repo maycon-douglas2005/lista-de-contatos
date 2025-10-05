42cc72ecc6380bb76dc15864c9216d29e6a9b30c
maycon-douglas2005 <mayconchaves2005@gmail.com>
2025-10-05 15:51:10 -0300

    style: atualiza grid da main da pagina index

--- public/index.php
+++ public/index.php
@@ -3,23 +3,26 @@ $baseUrl = '/Projetos de Programação/lista_de_contatos/public/index.php';
 ?>
 
 
-<body>
+<body class="d-flex flex-column vh-100">
     <?php require_once '../app/Views/partials/header.php' ?>
 
 
 
-    <main class="container-fluid">
-        <div class="apresentacao row d-flex flex-row align-items-center justify-content-center">
+    <main class="container-fluid  flex-fill">
+        <div class="apresentacao row d-flex flex-row align-items-center" style="height: 100%;">
 
-            <section class="tituloEparagrafo col-6">
+            <section class="tituloEparagrafo col-7 d-flex flex-column align-items-center">
                 <h1>Bem-Vindo A Sua Lista De Contatos</h1>
                 <p>Gerencie seus contatos de maneira simples e eficiente</p>
-                <button class="btn btn-outline-primary">Cadastrar-se</button>
-                <button class="btn btn-outline-primary">Logar</button>
+                <div class="botoes">
+                    <button class="btn btn-outline-primary">Cadastrar-se</button>
+                    <button class="btn btn-outline-primary">Logar</button>
+                </div>
+
             </section>
 
-            <img src="images/home_site.png" class="img-fluid col-6" alt="">
-
+            <img src="images/home_site.png" class="img-fluid col-5 shadow" alt="">
+        </div>
     </main>
 
 
